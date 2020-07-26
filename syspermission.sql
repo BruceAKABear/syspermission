6da3938ddbeed4376a2f905d66b5012af9b50736
dengyi <dengyi@dengyi.pro>
2020-07-26 22:44:34 +0800

    init

--- syspermission.sql
+++ syspermission.sql
@@ -11,11 +11,40 @@
  Target Server Version : 50717
  File Encoding         : 65001
 
- Date: 19/07/2020 22:20:26
+ Date: 26/07/2020 22:44:09
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for log_error
+-- ----------------------------
+DROP TABLE IF EXISTS `log_error`;
+CREATE TABLE `log_error`  (
+  `id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '异常日志表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for log_operate
+-- ----------------------------
+DROP TABLE IF EXISTS `log_operate`;
+CREATE TABLE `log_operate`  (
+  `id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键',
+  `op_module` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作模块',
+  `op_type` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作类型',
+  `op_desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作描述',
+  `op_req_parm` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '请求参数',
+  `op_res_parm` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '响应参数',
+  `op_user_id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作人id',
+  `op_user_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作人名字',
+  `op_method` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作请求方法',
+  `op_uri` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '请求uri',
+  `op_ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '请求ip',
+  `op_time` datetime(0) NULL DEFAULT NULL COMMENT '操作时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '操作日志表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for pe_permission_api
@@ -30,6 +59,11 @@ CREATE TABLE `pe_permission_api`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of pe_permission_api
+-- ----------------------------
+INSERT INTO `pe_permission_api` VALUES ('1284790628713631745', '/abcd/abcd', 'post', 1);
+
+-- ----------------------------
 -- Table structure for pe_permission_button
 -- ----------------------------
 DROP TABLE IF EXISTS `pe_permission_button`;
@@ -42,6 +76,11 @@ CREATE TABLE `pe_permission_button`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of pe_permission_button
+-- ----------------------------
+INSERT INTO `pe_permission_button` VALUES ('1284790443967123458', 'abcdfg', 'ddddd.icon', 'true');
+
+-- ----------------------------
 -- Table structure for pe_permission_menu
 -- ----------------------------
 DROP TABLE IF EXISTS `pe_permission_menu`;
@@ -51,6 +90,11 @@ CREATE TABLE `pe_permission_menu`  (
   `menu_order` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '序号',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of pe_permission_menu
+-- ----------------------------
+INSERT INTO `pe_permission_menu` VALUES ('1284790259853955073', 'menu_icon', '1');
 
 -- ----------------------------
 -- Table structure for t_permission
@@ -68,6 +112,16 @@ CREATE TABLE `t_permission`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of t_permission
+-- ----------------------------
+INSERT INTO `t_permission` VALUES ('112233', '用户管理', 1, 'menu2', '用户管理', NULL, NULL);
+INSERT INTO `t_permission` VALUES ('1284790259853955073', '权限管理', 1, 'menu_1', '新增菜单权限', NULL, NULL);
+INSERT INTO `t_permission` VALUES ('1284790443967123458', '菜单权限1-1', 1, 'menu_1-1', '菜单1-1', '1284790259853955073', NULL);
+INSERT INTO `t_permission` VALUES ('1284790628713631745', '菜单权限1-2', 1, 'menu_1-2', '菜单1-2', '1284790259853955073', NULL);
+INSERT INTO `t_permission` VALUES ('223344', '新增用户', 1, 'menu2-1', '新增用户', '112233', NULL);
+INSERT INTO `t_permission` VALUES ('334455', '用户列表', 1, '/userlist', '用户列表', '112233', NULL);
+
+-- ----------------------------
 -- Table structure for t_role
 -- ----------------------------
 DROP TABLE IF EXISTS `t_role`;
@@ -77,6 +131,12 @@ CREATE TABLE `t_role`  (
   `description` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_role
+-- ----------------------------
+INSERT INTO `t_role` VALUES ('1284763240269316097', '角色二', '角色二的描述');
+INSERT INTO `t_role` VALUES ('1284774887100694529', '角色一', '角色一的描述');
 
 -- ----------------------------
 -- Table structure for t_role_permission
@@ -90,15 +150,27 @@ CREATE TABLE `t_role_permission`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of t_role_permission
+-- ----------------------------
+INSERT INTO `t_role_permission` VALUES ('1284852155395248129', '1284774887100694529', '1284790259853955073');
+
+-- ----------------------------
 -- Table structure for t_user
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user`  (
   `id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键',
-  `username` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
-  `password` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `phone_number` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(80) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `is_admin` tinyint(1) NULL DEFAULT NULL COMMENT '是否是管理员',
+  `nick_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '昵称',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES ('112233', '17317539623', '$2a$10$9.7GMwP9OCNGQ0RKaXOjyetDD5r/oiYc.qUc4DhGEyijBbMTLWb42', 1, '大熊熊');
 
 -- ----------------------------
 -- Table structure for t_user_role
@@ -110,5 +182,10 @@ CREATE TABLE `t_user_role`  (
   `role_id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user_role
+-- ----------------------------
+INSERT INTO `t_user_role` VALUES ('1284804424962326529', '112233', '1284763240269316097');
 
 SET FOREIGN_KEY_CHECKS = 1;
